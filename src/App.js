6c0b49f14a3d6116160eb9fcda1f7e333b8c20ac
iwasaki2310 <118096073+iwasaki-2310@users.noreply.github.com>
2023-10-13 08:46:34 +0000

    初期設定

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const []  
+  const [count, setCount] =useState(0);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
